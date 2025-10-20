4483177b7f403848f0344e392d990a51ec856829
ketankulkarni2791@gmail.com <ketankulkarni2791@gmail.com>
2025-10-20 17:43:38 +0530

    fix(terraform): use db_name for aws_db_instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -107,7 +107,7 @@ resource "aws_db_instance" "postgres" {
   engine              = "postgres"
   engine_version      = "15.3"
   instance_class      = var.db_instance_class
-  name                = var.db_name
+  db_name             = var.db_name
   username            = var.db_username
   password            = var.db_password
   skip_final_snapshot = true
